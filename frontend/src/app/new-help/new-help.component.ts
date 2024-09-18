4b0a4a434dc6196c6e146f22ae1ac5d60fea1528
Pablo Pessanha <pablopessanh@gmail.com>
2024-09-18 19:54:56 -0300

    feat(frontend): adds options on help creation

--- frontend/src/app/new-help/new-help.component.ts
+++ frontend/src/app/new-help/new-help.component.ts
@@ -4,15 +4,24 @@ import { Router, RouterOutlet } from '@angular/router';
 import { UserService } from '@/services/user.service';
 import { HeaderComponent } from '@/components/Header/header.component';
 import { NewHelpStepsComponent } from '@/components/NewHelpSteps/new-help-steps.componen';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NewHelpStepsComponent],
+  imports: [RouterOutlet, HeaderComponent, NewHelpStepsComponent, SvgIconComponent],
   providers: [UserService],
   templateUrl: './new-help.component.html',
 })
 export class NewHelpComponent {
   readonly logged = this.userSignal.select('logged');
+  readonly options = [
+    { name: 'Jogos', icon: 'public/assets/rocket.svg' },
+    { name: 'Saúde', icon: 'public/assets/heart.svg' },
+    { name: 'Música', icon: 'public/assets/headphones.svg' },
+    { name: 'Reforma', icon: 'public/assets/home.svg' },
+    { name: 'Emergência', icon: 'public/assets/life-buoy.svg' },
+    { name: 'Hospitalar', icon: 'public/assets/activity.svg' },
+  ];
 
   constructor(
     private userSignal: UserService,
