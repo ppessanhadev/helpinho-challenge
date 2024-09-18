0de059ff76a934f91cf9b559125c8fd1b4e3015a
Pablo Pessanha <pablopessanh@gmail.com>
2024-09-17 22:47:44 -0300

    chore(frontend): removes not-found page

--- frontend/src/app/not-found/not-found.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-
-@Component({
-  standalone: true,
-  imports: [RouterOutlet, RouterModule],
-  templateUrl: './not-found.component.html',
-})
-export class NotFoundComponent {}
