9d8b802a9edae7b019f4737204ef1cb9ea3dcbd1
Pablo Pessanha <pablopessanh@gmail.com>
2024-09-21 11:34:58 -0300

    feat(backend): adds thrtottler and multer

--- backend/src/presentation/controllers/controller.module.ts
+++ backend/src/presentation/controllers/controller.module.ts
@@ -1,5 +1,27 @@
 import { Module } from '@nestjs/common';
 import { UserControllerModule } from './user/module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
-@Module({ imports: [UserControllerModule] })
+@Module({
+  imports: [
+    UserControllerModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
+})
 export class ControllerModule {}
